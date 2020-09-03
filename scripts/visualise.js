969c85be6b5b677cb1ce76c245e17a5a412258e9
jctwizard <jamie.ct.wood@gmail.com>
2020-09-03 09:48:48 +0100

    Add canvas visualisation to survey

--- scripts/visualise.js
+++ scripts/visualise.js
@@ -1,9 +1,34 @@
 function initSurvey(surveyIndex)
 {
-  viewSurveyResults(surveyIndex);
+  //viewSurveyResults(surveyIndex);
+  visualiseSurveyResults(surveyIndex);
 }
 
 function initNoSurvey()
 {
   displaySurveys();
+}
+
+function visualiseSurveyResults(surveyIndex)
+{
+  var canvas = document.getElementById("resultsCanvas");
+
+  canvas.style.visibility = "visible";
+
+  var context = canvas.getContext('2d');
+  var centerX = canvas.width / 2;
+  var centerY = canvas.height / 2;
+
+  var radius = 70;
+
+  context.beginPath();
+  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'green';
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = '#003300';
+  context.stroke();
+  
+  context.font = "30px arialRounded";
+  context.fillText(getSurveyName(surveyIndex), centerX, centerY); 
 }
