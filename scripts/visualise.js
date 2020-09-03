a9d2f07cb7f4b3174673d6e8df0c4194ab2af387
jctwizard <jamie.ct.wood@gmail.com>
2020-09-03 10:05:05 +0100

    use canvas size to scale text

--- scripts/visualise.js
+++ scripts/visualise.js
@@ -19,19 +19,23 @@ function visualiseSurveyResults(surveyIndex)
 
   var context = canvas.getContext('2d');
 
+  var width = canvas.width;
+  var height = canvas.height;
   var centerX = canvas.width / 2;
   var centerY = canvas.height / 2;
 
-  var radius = 70;
+  var radius = (width < height ? width : height) / 4;
 
   context.beginPath();
   context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context.fillStyle = 'green';
+  context.fillStyle = 'yellow';
   context.fill();
   context.lineWidth = 5;
   context.strokeStyle = '#003300';
   context.stroke();
   
-  context.font = "30px arialRounded";
+  context.font = (radius / 2).toString() + "px arialRounded";
+  context.fillStyle = 'black';
+  context.textAlign = "center"; 
   context.fillText(getSurveyName(surveyIndex), centerX, centerY); 
 }
